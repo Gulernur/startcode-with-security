5b1983f97304772a33021b50b0be232c9fde75b0
gulernurdonmez <gule0153@stud.kea.dk>
2022-09-01 08:39:54 +0200

    minor changes

--- src/main/java/dat3/cars/service/CarService.java
+++ src/main/java/dat3/cars/service/CarService.java
@@ -3,9 +3,7 @@ package dat3.cars.service;
 
 import dat3.cars.dto.CarRequest;
 import dat3.cars.dto.CarResponse;
-import dat3.cars.dto.MemberResponse;
 import dat3.cars.entity.Car;
-import dat3.cars.entity.Member;
 import dat3.cars.repository.CarRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,10 +21,9 @@ public class CarService {
 
 
     public List<CarResponse> getCars() {
-        //List<Car> cars = carRepository.findAll();
-        //List<CarResponse> response = cars.stream().map(member -> new CarResponse(cars,false)).collect(Collectors.toList());
-        //return response;
-        return null;
+        List<Car> cars = carRepository.findAll();
+        List<CarResponse> response = cars.stream().map(car -> new CarResponse(car,false)).collect(Collectors.toList());
+        return response;
     }
 
     public static CarResponse addCar(CarRequest body) {
