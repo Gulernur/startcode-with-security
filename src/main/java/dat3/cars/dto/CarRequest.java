ff2a1a0943606d7795d14865347cbfbd532a8c7b
gulernurdonmez <gule0153@stud.kea.dk>
2022-09-01 08:39:00 +0200

    completed classes

--- src/main/java/dat3/cars/dto/CarRequest.java
+++ src/main/java/dat3/cars/dto/CarRequest.java
@@ -1,4 +1,22 @@
 package dat3.cars.dto;
 
+import dat3.cars.entity.Car;
+
 public class CarRequest {
+
+    String brand;
+    String model;
+    double pricePrDay;
+    double bestDiscount;
+
+    public static Car getCarEntity(CarRequest c){
+        return new Car(c.brand, c.model, c.pricePrDay, c.bestDiscount);
+    }
+
+    public CarRequest(Car c) {
+        this.brand = c.getBrand();
+        this.model = c.getModel();
+        this.pricePrDay = c.getPricePrDay();
+        this.bestDiscount = c.getBestDiscount();
+    }
 }
--- src/main/java/dat3/cars/dto/CarResponse.java
+++ src/main/java/dat3/cars/dto/CarResponse.java
@@ -1,4 +1,31 @@
 package dat3.cars.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import dat3.cars.entity.Car;
+
+import java.time.LocalDateTime;
+
 public class CarResponse {
+
+    String brand;
+    String model;
+    double pricePrDay;
+    double bestDiscount;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    LocalDateTime created;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    LocalDateTime edited;
+
+    public CarResponse(Car c, boolean includeAll) {
+        this.brand = c.getBrand();
+        this.model = c.getModel();
+        this.pricePrDay = c.getPricePrDay();
+        this.bestDiscount = c.getBestDiscount();
+        if (includeAll) {
+            this.created = c.getCreated();
+            this.edited = c.getEdited();
+        }
+    }
 }
